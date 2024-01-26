a604a417228f0f53da8f171c19a6033b051c4d3e
bjoreno <bjodegens@hotmail.com>
2024-01-26 14:25:17 +0100

    navbar bg purple

--- views/layouts/index_layout.php
+++ views/layouts/index_layout.php
@@ -32,7 +32,7 @@ AppAsset::register($this);
             'brandLabel' => Yii::$app->name,
             'brandUrl' => Yii::$app->homeUrl,
             'options' => [
-                'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top custom-font',
+                'class' => 'navbar navbar-expand-md navbar-dark bg-purple fixed-top custom-font',
             ],
         ]);
         $menuItems = [
